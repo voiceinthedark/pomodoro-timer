136e06a07cdb0a3407faee3f939ea72ebbd021f1
voiceinthedark <darkrisingforce@gmail.com>
2021-12-20 21:54:15 +0200

    - Add primary effect of stroke decreasing around the timer

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ let minutesInput = document.querySelector('#minutesInput');
 let secondsInput = document.querySelector('#secondsInput');
 // get the start button
 const btnStart = document.querySelector('.start');
+// get the circle
+const circle = document.querySelector('#circle');
+let radiusOfTimerCircle = circle.getAttribute('r');
+// Length of the arc 2*π*r 
+/* we need this for stroke-dasharray value */
+let lengthOfArc = 2 * Math.PI * radiusOfTimerCircle; // 1595.929068023615
+
+
 
 // starting with original time limit of 15 minutes
-let time_limit = 15 * 60;
+let time_limit = 1 * 60;
 // the time that has passed since starting
 let time_passed = 0;
 // initialy time left is equal to the time limit
@@ -41,10 +49,30 @@ function startTimer() {
         minutesInput.value = minutes;
         secondsInput.value = seconds;
 
+        setCircleDasharray();
+
     }, 1000);
     
+}
+
+// Divides time left by the defined time limit.
+function calculateTimeFraction() {
+  return time_left / time_limit;
+}
+    
+// Update the dasharray value as time passes, starting with 283
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * lengthOfArc
+  ).toFixed(0)} ${lengthOfArc}`;
+  document
+    .querySelector("circle")
+    .setAttribute("stroke-dasharray", circleDasharray);
 }
 
 btnStart.addEventListener('click', () => {
     startTimer();
 })
+
+
+
